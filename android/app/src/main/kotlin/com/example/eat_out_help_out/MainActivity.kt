65adf71c3d69b3215a6955ec0a2bb21d45391937
Fatemah Tahavori <fatemah.tahavori@continental-corporation.com>
2020-08-10 21:01:42 +0100

    eatOutHelpOutApp

---
+++ android/app/src/main/kotlin/com/example/eat_out_help_out/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.eat_out_help_out
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(50.8442448, -0.1440247);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('eat out help out'),
+          backgroundColor: Colors.blue[700],
+        ),
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
+        ),
+      ),
+    );
+  }
+}
