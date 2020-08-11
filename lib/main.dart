3e78b83376d13e53f57ab4d71acbc022b746d5dd
David Chanin <chanindav@gmail.com>
2020-08-11 01:23:40 +0100

    getting app working with server

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'src/locations.dart' as locations;
+import 'package:http/http.dart' as http;
+import 'dart:convert' as json;
+import 'package:just_debounce_it/just_debounce_it.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,22 +15,36 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   final Map<String, Marker> _markers = {};
-  Future<void> _onMapCreated(GoogleMapController controller) async {
-    final googleOffices = await locations.getGoogleOffices();
-    setState(() {
+  GoogleMapController _controller;
+  Future<void> _onMapCreatedOrChanged(GoogleMapController controller) async {
+    _controller = controller;
+    final visibleRegion = await controller.getVisibleRegion();
+    final lat1 = visibleRegion.southwest.latitude;
+    final lng1 = visibleRegion.southwest.longitude;
+    final lat2 = visibleRegion.northeast.latitude;
+    final lng2 = visibleRegion.northeast.longitude;
+    final request = await http.get(
+        'https://eat-out-help-out.herokuapp.com/query?lat1=$lat1&lng1=$lng1&lat2=$lat2&lng2=$lng2');
+    final data = json.jsonDecode(request.body);
+    await setState(() {
       _markers.clear();
-      for (1) {
+      for (final result in data['results']) {
         final marker = Marker(
-          markerId: MarkerId(results.name),
-          position: LatLng(results.lat, results.lng),
+          markerId: MarkerId(result['name']),
+          position: LatLng(result['lat'], result['lng']),
           infoWindow: InfoWindow(
-            title: results.name,
-            snippet: results.address,
+            title: result['name'],
+            snippet:
+                'address goes here', // todo: format address as a string: result['address'],
           ),
         );
-        _markers[results.name] = marker;
+        _markers[result['name']] = marker;
       }
     });
+  }
+
+  void _onMapCreatedOrChangedDebounced(CameraPosition cameraPosition) async {
+    Debounce.milliseconds(1000, _onMapCreatedOrChanged, [_controller]);
   }
 
   @override
@@ -41,7 +57,8 @@ class _MyAppState extends State<MyApp> {
           backgroundColor: Colors.blue[700],
         ),
         body: GoogleMap(
-          onMapCreated: _onMapCreated,
+          onMapCreated: _onMapCreatedOrChanged,
+          onCameraMove: _onMapCreatedOrChangedDebounced,
           initialCameraPosition: CameraPosition(
             target: const LatLng(0, 0),
             zoom: 11.0,
